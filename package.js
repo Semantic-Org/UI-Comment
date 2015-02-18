617cc6504694501b75897a94ef712df653b5a851
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:32:20 -0500

    Updated component to version 1.9.1

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-comment',
+  summary : 'Semantic UI - Comment (official): Single component release of comment',
+  version : '1.9.1',
+  git     : 'git://github.com/Semantic-Org/UI-Comment.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
