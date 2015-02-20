eaea53b74259e7192939a6143a63ca65aeae1c56
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:15:32 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,14 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-comment',
-  summary : 'Semantic UI - Comment (official): Single component release of comment',
-  version : '1.9.2',
+  summary : 'Semantic UI - Comment: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Comment.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'comment.css'
+  ], 'client');
 });
