5bed3fee318546203bb34a303f8bcc44ff76d963
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:40:47 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-comment',
   summary : 'Semantic UI - Comment: Single component release',
-  version : '2.2.8',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Comment.git',
 });
 
